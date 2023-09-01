97e56acf4f133b6463e93ff301c86bca11094f90
Viktoria <viktoria.korobjina@yandex.ru>
2023-09-01 19:48:28 +0300

    Example005

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите свое имя: ");
+string username = Console.ReadLine();
+if (username == "Masha")
+{
+    Console.WriteLine("Здорово, Маха!");
+}
+else
+{
+    Console.Write("Здравствуй, ");
+    Console.WriteLine(username);
+}
